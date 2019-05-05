ac81784c92c6550443bd1edec75cc91d26e7d1ec
saypop <tome.m.dj@gmail.com>
2019-05-05 17:14:29 +0100

    Bug fixes.

--- popup/choose_beast.js
+++ popup/choose_beast.js
@@ -3,29 +3,29 @@
  * except for elements that have the "beastify-image" class.
  */
  const hidePage = `body > :not(.beastify-image) {
-   display: none;
- }`;
+                    display: none;
+                  }`;
 
  /**
  * Listen for clicks on the buttons, and send the appropriate message to
  * the content script in the page.
  */
  function listenForClicks() {
-   document.addEventListener("click", (e) => {
+  document.addEventListener("click", (e) => {
 
      /**
      * Given the name of a beast, get the URL to the corresponding image.
      */
      function beastNameToURL(beastName) {
-       switch (beastName) {
-         case: "Frog":
+      switch (beastName) {
+        case "Frog":
           return browser.extension.getURL("beasts/frog.jpg");
-         case: "Snake":
+        case "Snake":
           return browser.extension.getURL("beasts/snake.jpg");
-         case: "Turtle":
+        case "Turtle":
           return browser.extension.getURL("beasts/turtle.jpg");
-       }
-     }
+      }
+    }
 
      /**
      * Insert the page-hiding CSS into the active tab,
@@ -33,14 +33,14 @@
      * send a "beastify" message to the content script in the active tab.
      */
      function beastify(tabs) {
-       browser.tabs.removeCSS({code: hidePage}).then(() => {
-         let url = beastNameToURL(e.target.textContent);
-         browser.tabs.sendMessage(tabs[0].id, {
-           command: "beastify",
-           beastURL: url
-         });
-       });
-     }
+      browser.tabs.insertCSS({code: hidePage}).then(() => {
+        let url = beastNameToURL(e.target.textContent);
+        browser.tabs.sendMessage(tabs[0].id, {
+          command: "beastify",
+          beastURL: url
+        });
+      });
+    }
 
      /**
      * Remove the page-hiding CSS from the active tab,
